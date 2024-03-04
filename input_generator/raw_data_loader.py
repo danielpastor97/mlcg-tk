35c8bbacd0ca93fbf26785927739c9b4c6f15e77
kbno <bonneau.klara@gmail.com>
2024-03-04 09:57:08 +0100

    removed wrong force conversion in charmm dataset loaders

--- input_generator/raw_data_loader.py
+++ input_generator/raw_data_loader.py
@@ -299,8 +299,6 @@ class Trpcage_loader(DatasetLoader):
         for cfn, ffn in zip(coords_fns, forces_fns):
             force = np.load(ffn)
             coord = np.load(cfn)
-            coord = coord  # * 10
-            force = force / 4.184  # convert to from kJ/mol/ang to kcal/mol/ang
             assert coord.shape == force.shape
             aa_coord_list.append(coord)
             aa_force_list.append(force)
@@ -336,8 +334,6 @@ class Cln_loader(DatasetLoader):
         for cfn, ffn in zip(coords_fns, forces_fns):
             force = np.load(ffn)
             coord = np.load(cfn)
-            coord = coord  # * 10
-            force = force / 4.184  # convert to from kJ/mol/ang to kcal/mol/ang
             assert coord.shape == force.shape
             aa_coord_list.append(coord)
             aa_force_list.append(force)
