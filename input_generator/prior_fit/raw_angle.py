c587a728990fd196dc0f5e6d8134bfaf4a71ecfc
Daniel Pastor Ramirez <daniel.ramirezecheme@ucalgary.ca>
2025-03-26 13:35:46 -0600

    fit priors ok

--- input_generator/prior_fit/raw_angle.py
+++ input_generator/prior_fit/raw_angle.py
@@ -2,11 +2,10 @@ from mlcg.nn.prior import Harmonic
 from .utils import compute_raw_angles
  
 class HarmonicRawAngle(Harmonic):
-    name = "Angles"
     _order = 3
 
-    def __init__(self, statistics) -> None:
-        super(HarmonicRawAngle, self).__init__(statistics, HarmonicRawAngle.name)
+    def __init__(self, statistics, name: str = "Angles") -> None:
+        super(HarmonicRawAngle, self).__init__(statistics, name=name)
 
     @staticmethod
     def neighbor_list(topology) -> dict:
